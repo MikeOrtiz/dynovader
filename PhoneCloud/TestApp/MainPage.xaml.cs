5d73b1a699960a1c1f7cfb693bd5bc8d912ff1ed
Lu Li <lulu.li1989@gmail.com>
2011-03-24 18:34:05 -0700

    make sql azure working

---
+++ PhoneCloud/TestApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+using TestApp.MyScienceService;
+
+namespace TestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyScienceServiceClient client = new MyScienceServiceClient();
+            client.GetProjectsCompleted += new EventHandler<GetProjectsCompletedEventArgs>(client_GetProjectsCompleted);
+            client.GetProjectsAsync();
+        }
+
+        void client_GetProjectsCompleted(object sender, GetProjectsCompletedEventArgs e)
+        {
+            if (e.Result != null)
+            {
+                this.MainListBox.ItemsSource = e.Result;
+            }
+            this.MainListBox.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
---
+++ PhoneCloud/TierWebLayer/MyScienceService.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using TierDataLayer;
+
+namespace TierWebLayer
+{
+    [ServiceContract(Namespace = "")]
+    [SilverlightFaultBehavior]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class MyScienceService
+    {
+        [OperationContract]
+        public List<TierDataLayer.Project> GetProjects()
+        {
+            MyScienceEntities db = new MyScienceEntities();
+            var query = (from app in db.projects
+                         select new Project
+                         {
+                             ID = app.ID,
+                             Name = app.name,
+                             Description = app.description,
+                             Form = app.form,
+                             Owner = app.owner
+                         });
+            return query.ToList();
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
