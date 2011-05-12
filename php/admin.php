7786030372d0a0594bfe8c13c564e3d0aac81c3f
unknown <Mike@.(none)>
2011-05-12 02:20:38 -0700

    Photo submissions optional. Other UI changes based on user feedback.

--- php/admin.php
+++ php/admin.php
@@ -60,8 +60,8 @@ if(isset($_POST['coordname']))
 					}
 				}
 			}
-			$values = substr($values, 0, -1);
-			$values .= "]";
+			$values .= "{\"type\":\"Photo\",\"value\":\"".$_POST['photo']."\"}]";
+			
 			$query = "INSERT INTO projects(name, description, owner, form) VALUES('".$_POST['apptitle']."','".$_POST['description']."',$coordid,'".$values."')";
 			$result = sqlsrv_query($conn, $query);
 			echo "Your project <i>".$_POST['apptitle']."</i> was added successfully! It is currently under review.";
@@ -81,37 +81,6 @@ if(isset($_POST['coordname']))
 <script src="http://jqueryui.com/ui/jquery.ui.sortable.js"></script> 
 <link rel="stylesheet" href="http://jqueryui.com/demos/demos.css">
 <script>
-$(document).ready(function() {
-  $("#text1").change(function() {
-    if($("#text1").is(":checked")){
-		$("#tf2").show();
-	}
-	else{
-		$("#tf2").hide();
-	}
-  });
-  $("#text2").change(function() {
-    if($("#text2").is(":checked")){
-		$("#tf3").show();
-		$("#text1").attr("disabled","true");
-	}
-	else{
-		$("#tf3").hide();
-		$("#text1").attr("disabled","");
-	}
-  });
-   $("#text3").change(function() {
-    if($("#text3").is(":checked")){
-		$("#tf4").show();
-		$("#text2").attr("disabled","true");
-	}
-	else{
-		$("#tf4").hide();
-		$("#text2").attr("disabled","");
-	}
-  });
-});
-
 $(function() {
 		$( "#sortable" ).sortable();
 		$( "#sortable" ).disableSelection();
@@ -164,6 +133,22 @@ function updateTitle(){
 	document.getElementById('apptitle').innerHTML = userInput;
 }
 
+var photoOption;
+
+//Updates whether user wants photoOption set to 'Y' or 'N'
+function updatePhotoOption(){
+	var x = document.getElementById('picturequestion');
+	if (x.value == 'Add Picture Capture') {
+		x.value = 'Remove Picture Capture';
+		document.getElementById('photobuttons').style.display = 'block';
+		photoOption = 'Y';
+	} else {
+		x.value = 'Add Picture Capture';
+		document.getElementById('photobuttons').style.display = 'none';
+		photoOption = 'N';
+	}
+	document.getElementById('photopost').value = photoOption;
+}
    
 </script>
 <style type="text/css" media="screen">
@@ -229,6 +214,12 @@ float: left;
 
 #dragcontent{
 	height: 386px;
+	width: 263px;
+	overflow-y: hidden;
+}
+
+#dragcontent:hover
+{
 	width: 280px;
 	overflow-y: scroll;
 }
@@ -290,24 +281,12 @@ body{
 	float: right;
 }
 
-#photoalign1
-{
-	position: relative;
-	left: 85px;
-}
-
-#photoalign2
-{
-	position: relative;
-	left: 76px;
-}
-
 .floatright
 {
 	float: right;
 }
 
-#cbutton
+#bottombuttons
 {
 	margin-top: 20px;
 	text-align: center;
@@ -316,9 +295,24 @@ body{
 .moveleft
 {
 	position: absolute;
-	width: 295px;
+	width: 320px;
 	right: 400px;
-	bottom: 0px;
+	bottom: 100px;
+}
+
+.question:hover
+{
+	background-color:lightgray;
+}
+
+input
+{
+	background-color:transparent;
+}
+
+.ui-icon ui-icon-closethick:hover
+{
+	cursor:default;
 }
 
 
@@ -346,24 +340,6 @@ body{
 <div class="phone">
 	<img src="phone.png"/></img>
 	<form method="POST" action="admin.php">	
-		<div class="phone_content">
-			<div id="apptitle" name="apptitle" size="30" value="Enter App Title Here"></div>
-			<div id="menubar">
-				<span class="gray">Submission</span> Da
-			</div>
-			<div id="dragcontent">
-				<div id="sortable">
-					<div id="writeroot"></li>
-				</div>
-				<span id="photoalign1"><input type="button" value="Take a Photo"></span><br />
-				<span id="photoalign2"><button type="button">Choose a Photo</button></span>
-				<div id="cbutton">
-					<button type="button">Save Only</button><br />
-					<button type="button">Submit</button>
-				</div>
-			</div> 
-		</div>
-	
 		<div class="moveleft">
 			<h1>Launch a Project</h1>
 			<span class="columnleft">App Title: </span><span class="columnright">
@@ -385,8 +361,8 @@ body{
 			-->
 			<input type="button" value="Check Question" onclick="moreCheck()"/> <br />
 			<input type="button" value="Radio Question" onclick="moreRadio()"/> <br />
-			<!--<input type="button" value="Add Picture Capture" onclick="morePicture()"/> <br /><br />-->
-			<br />
+			<input id="picturequestion" type="button" value="Remove Picture Capture" onclick="updatePhotoOption()"/> <br /><br />
+			<input type="text" id="photopost" name="photo" value="Y" style="display:none">
 			<input type="submit" value="Submit App!" />	
 			<h3>Instructions:</h3>
 			<ol>
@@ -394,9 +370,30 @@ body{
 				<li>Fill out Description, Name, and Email above</li>
 				<li>Add and remove questions with buttons above</li>
 				<li>Edit questions and options directly on phone</li>
+				<li>Drag and drop questions to edit order</li>
 				<li>Press Submit App!</li>
 			</ol>	
 			<br /><br />
+		</div>
+		
+		<div class="phone_content">
+			<div id="apptitle" name="apptitle" size="30" value="Enter App Title Here"></div>
+			<div id="menubar">
+				<span class="gray">Submission</span> Da
+			</div>
+			<div id="dragcontent">
+				<div id="sortable">
+					<span id="writeroot"></span>
+				</div>
+				<div id="bottombuttons">
+					<div id="photobuttons" style="display:block">
+						<button type="button" disabled="disabled">Take a Photo</button><br />
+						<button type="button" disabled="disabled">Choose a Photo</button><br /><br />
+					</div>
+					<button type="button" disabled="disabled">Save Only</button><br />
+					<button type="button" disabled="disabled">Submit</button>
+				</div>
+			</div> 
 		</div>
 	</form>
 </div>
@@ -404,43 +401,54 @@ body{
 
 <div><?=$msg?></div>
 
-<div id="textroot" style="display: none">
-	<input class="blend" name="textq" size="29" maxlength="35" value="Enter your question here.">
+<div class="question" id="textroot" style="display: none">
+	<input class="blend" name="textq" size="29" maxlength="35" value="Enter your question here."
+	onFocus="if(this.value == 'Enter your question here.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter your question here.';}">
 	<span class="floatright">
 		<span class="ui-icon ui-icon-closethick" onclick="this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);" /></span>
 	</span><br />
-	<input name="texta" disabled="disabled" value="User answers here.">
-	<div class="floatright">
-		<span class="ui-icon ui-icon-arrowthick-2-n-s">
-	</div>
+	<input class="textanswer" name="texta" disabled="disabled">
+	<span class="floatright">
+		<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
+	</span>
 </div>
 
-<div id="checkroot" style="display: none">
-	<input class="blend" name="checkq" size="29" maxlength="35" value="Enter your question here.">
+<div class="question" id="checkroot" style="display: none">
+	<input class="blend" name="checkq" size="29" maxlength="35" value="Enter your question here."
+	onFocus="if(this.value == 'Enter your question here.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter your question here.';}">
 	<span class="floatright">
 		<span class="ui-icon ui-icon-closethick" onclick="this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);" /></span>
 	</span><br />
-	<input type="checkbox" name="check1" disabled="disabled"/> <input class="blend" name="checkA1" size="29" maxlength="35" value="Enter check option 1.">
+	<input type="checkbox" name="check1" disabled="disabled"/> <input class="blend" name="checkA1" size="29" maxlength="35" value="Enter check option 1."
+	onFocus="if(this.value == 'Enter check option 1.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter check option 1.';}">
 	<div class="floatright">
 		<span class="ui-icon ui-icon-arrowthick-2-n-s">
 	</div><br />
-	<input type="checkbox" name="check2" disabled="disabled"/> <input class="blend" name="checkA2" size="30" maxlength="35" value="Enter check option 2."><br />
-	<input type="checkbox" name="check3" disabled="disabled"/> <input class="blend" name="checkA3" size="30" maxlength="35" value="Enter check option 3."><br />
-	<input type="checkbox" name="check4" disabled="disabled"/> <input class="blend" name="checkA4" size="30" maxlength="35" value="Enter check option 4."><br />
+	<input type="checkbox" name="check2" disabled="disabled"/> <input class="blend" name="checkA2" size="29" maxlength="35" value="Enter check option 2."
+	onFocus="if(this.value == 'Enter check option 2.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter check option 2.';}"><br />
+	<input type="checkbox" name="check3" disabled="disabled"/> <input class="blend" name="checkA3" size="29" maxlength="35" value="Enter check option 3."
+	onFocus="if(this.value == 'Enter check option 3.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter check option 3.';}"><br />
+	<input type="checkbox" name="check4" disabled="disabled"/> <input class="blend" name="checkA4" size="29" maxlength="35" value="Enter check option 4."
+	onFocus="if(this.value == 'Enter check option 4.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter check option 4.';}"><br />
 </div>
  
-<div id="radioroot" style="display: none">
-	<input class="blend" name="checkq" size="29" maxlength="35" value="Enter your question here.">
+<div class="question" id="radioroot" style="display: none">
+	<input class="blend" name="checkq" size="29" maxlength="35" value="Enter your question here."
+	onFocus="if(this.value == 'Enter your question here.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter your question here.';}">
 	<span class="floatright">
 		<span class="ui-icon ui-icon-closethick" onclick="this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);" /></span>
 	</span><br />
-	<input type="radio" name="radio1" disabled="disabled"/> <input class="blend" name="radioA1" size="29" maxlength="35" value="Enter radio option 1.">
+	<input type="radio" name="radio1" disabled="disabled"/> <input class="blend" name="radioA1" size="29" maxlength="35" value="Enter radio option 1."
+	onFocus="if(this.value == 'Enter radio option 1.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter radio option 1.';}">
 	<div class="floatright">
 		<span class="ui-icon ui-icon-arrowthick-2-n-s">
 	</div><br />
-	<input type="radio" name="radio2" disabled="disabled"/> <input class="blend" name="radioA2" size="30" maxlength="35" value="Enter radio option 2."><br />
-	<input type="radio" name="radio3" disabled="disabled"/> <input class="blend" name="radioA3" size="30" maxlength="35" value="Enter radio option 3."><br />
-	<input type="radio" name="radio4" disabled="disabled"/> <input class="blend" name="radioA4" size="30" maxlength="35" value="Enter radio option 4."><br />
+	<input type="radio" name="radio2" disabled="disabled"/> <input class="blend" name="radioA2" size="29" maxlength="35" value="Enter radio option 2."
+	onFocus="if(this.value == 'Enter radio option 2.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter radio option 2.';}"><br />
+	<input type="radio" name="radio3" disabled="disabled"/> <input class="blend" name="radioA3" size="29" maxlength="35" value="Enter radio option 3."
+	onFocus="if(this.value == 'Enter radio option 3.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter radio option 3.';}"><br />
+	<input type="radio" name="radio4" disabled="disabled"/> <input class="blend" name="radioA4" size="29" maxlength="35" value="Enter radio option 4."
+	onFocus="if(this.value == 'Enter radio option 4.') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Enter radio option 4.';}"><br />
 </div>
 
 <div id="pictureroot" style="display: none">
@@ -450,33 +458,6 @@ body{
 	</span><br />
 	<span id="photoalign2"><button type="button">Choose a Photo</button></span>
 </div>
-
-
-<!-----
-<h1>Launch a Project</h1>
-<form action="admin.php" method="POST">
-<span class="columnleft">Project Name: </span><span class="columnright"><input type="text" name="appname"/></span><br/>
-<span class="columnleft">Description: </span><span class="columnright"><input type="text" name="description"/></span><br/>
-<span class="columnleft">GPS: </span><span class="columnright"><input type="checkbox" name="gps"/></span><br/>
-<span class="columnleft">Photo: </span><span class="columnright"><input type="checkbox" name="photo"/></span><br/>
-<span class="columnleft">Photo Instructions: </span><span class="columnright"><input type="text" name="photoinstructions"/></span><br/>
-<span class="columnleft">Text Field 1: </span><span class="columnright"><input id="text1" type="checkbox" name="textfield1"/></span><br/>
-<div id="tf2">
-<span class="columnleft">Text Field 1 Instructions: </span><span class="columnright"><input type="text" name="text1instructions"/></span><br/>
-<span class="columnleft">Text Field 2: </span><span class="columnright"><input id="text2" type="checkbox" name="textfield2"/></span><br/>
-</div>
-<div id="tf3">
-<span class="columnleft">Text Field 2 Instructions: </span><span class="columnright"><input type="text" name="text2instructions"/></span><br/>
-<span class="columnleft">Text Field 3: </span><span class="columnright"><input id="text3" type="checkbox" name="textfield3"/></span><br/>
-</div>
-<div id="tf4">
-<span class="columnleft">Text Field 3 Instructions: </span><span class="columnright"><input type="text" name="text3instructions"/></span><br/>
-</div>
-<span class="columnleft">Coordinator Name: </span><span class="columnright"><input type="text" name="coordname"/></span><br/>
-<span class="columnleft">Coordinator Email: </span><span class="columnright"><input type="text" name="coordemail"/></span><br/>
-<span class="columnright"><input type="submit" value="submit"/></span>
-</form>
--->
 </div>
 </div>
 </body>
