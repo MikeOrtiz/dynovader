c822342334ed617b795e52605ab509bf109b7ecd
Lu Li <lulu.li1989@gmail.com>
2011-05-23 16:28:13 -0700

    Left out changes

---
+++ MyScience/Begin/MyScience/LogoutMessageControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace MyScience
+{
+    public partial class LogoutMessageControl : UserControl
+    {
+        public LogoutMessageControl()
+        {
+            InitializeComponent();
+        }
+
+        private void logout_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).appReset();
+            App.popup.IsOpen = false;
+        }
+
+        private void stay_Click(object sender, RoutedEventArgs e)
+        {
+            App.popup.IsOpen = false;
+        }
+    }
+}
